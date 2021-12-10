3c6e660232701d076efc83480a8abfbfb12d2ab4
dankrzeminski32 <dankrzeminski32@gmail.com>
2021-12-10 08:31:06 -0600

    Elements are all formatted properly

--- index.js
+++ index.js
@@ -14,21 +14,22 @@ function getStocks() {
     .then((data) => {
       console.log(data);
       stockDate(data);
-      showIndex(data);
-      let upOrDown = stockUporDown(data[0].change);
-      console.log(upOrDown);
+      //showIndex(data);
+      //let upOrDown = stockUporDown(data[0].change);
+      //console.log(upOrDown);
+      appendData(data);
     })
     .catch((err) => {
       console.error(err);
     });
 }
 
-function showIndex(data) {
-  let row = document.querySelector(".index");
-  let index = trimFirstCharacter(data[0].symbol);
-  console.log(index);
-  row.innerHTML = index;
-}
+// function showIndex(data) {
+//   let row = document.querySelector(".index");
+//   let index = trimFirstCharacter(data[0].symbol);
+//   console.log(index);
+//   row.innerHTML = index;
+// }
 
 //used to test if you have the correct dates of objects in array
 function stockDate(data) {
@@ -48,4 +49,22 @@ function stockUporDown(change) {
   } else {
     return "images/Red_arrow_down.png";
   }
+}
+
+function appendData(data) {
+  let container = document.querySelector(".stock-widget");
+  data.forEach((element) => {
+    let index = trimFirstCharacter(element.symbol);
+    container.innerHTML += `<div class="index">${index}</div>
+    <div class="top-flex">
+      <div style="font-size: 5rem;">&#128315</div>
+      <div class="top-vertical-flex">
+        <p>${element.change.toFixed(2)}</p>
+        <p>${element.changesPercentage.toFixed(2)}%</p>
+      </div>
+    </div>
+    <div class="current-price">
+      ${element.price}
+    </div><hr>`;
+  });
 }
